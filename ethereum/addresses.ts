502e87cb800806c386b4820c623b8082084effc2
Jeremy Boetticher <me4jeremyboett@gmail.com>
2022-08-26 12:15:42 -0500

    Added polygon contract

--- ethereum/addresses.ts
+++ ethereum/addresses.ts
@@ -4,5 +4,5 @@ export default {
   [MoonbaseAlpha.chainId]: "0x522172f49d0e04bddb9bf14d66ca38619b42f19a",
   [FantomTestnet.chainId]: "0xfb8afead05b130796de04f00d9213a7bac2bc6c2",
   [AvalancheTestnet.chainId]: "0x827d4b6bd9660d1a4c42e4453307c8d3d5c1f051",
-  [Mumbai.chainId]: "" // Pending, it's actually screwing up in remix
+  [Mumbai.chainId]: "0x827d4B6BD9660d1A4c42E4453307C8D3D5C1F051"
 }
