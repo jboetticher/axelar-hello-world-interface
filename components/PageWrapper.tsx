90f7d8efb9a02c7ffa5c6e8819a03a9e133d95e8
Jeremy Boetticher <me4jeremyboett@gmail.com>
2022-12-02 09:20:24 -0600

    Added a page wrapper

---
+++ components/PageWrapper.tsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from 'react';
+import { Container, Button, Icon, Menu } from 'semantic-ui-react';
+import Head from 'next/head';
+import SendMessage from '../components/SendMessage';
+import Link from 'next/link';
+import { MoonbaseAlpha, FantomTestnet, AvalancheTestnet, Mumbai, useEthers } from '@usedapp/core';
+
+const PageWrapper = ({children}) => {
+  // Initial State
+  const { account, chainId, error, deactivate, activateBrowserWallet } = useEthers();
+  const isConnected = account !== undefined;
+
+  const networkName = (() => {
+    if (chainId === undefined && error.name === 'ChainIdError') return 'Not Supported';
+    switch (chainId) {
+      case undefined: return 'Not Connected';
+      case MoonbaseAlpha.chainId: return 'Moonbase Alpha';
+      case FantomTestnet.chainId: return 'Fantom Testnet';
+      case AvalancheTestnet.chainId: return 'Avalanche Fuji';
+      case Mumbai.chainId: return 'Mumbai';
+      default: return 'Not Supported';
+    }
+  })();
+
+  return (
+    <Container>
+      <Head>
+        <title>Axelar {'<'}{'>'} Moonbase Hello World</title>
+        <link rel='icon' type='image/png' sizes='32x32' href='/favicon.png' />
+        <link
+          rel='stylesheet'
+          href='//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css'
+        />
+      </Head>
+      <div style={{ paddingTop: '10px' }}></div>
+      <Menu>
+        <Link href='/'>
+          <a className='item'>Moonbase Alpha Multichain Hello World</a>
+        </Link>
+        <Menu.Menu position='right'>
+          <a className='item'> {account} </a>
+          {isConnected ? (
+            <Button floated='right' icon labelPosition='left' color='green' onClick={deactivate}>
+              <Icon name='check'></Icon>
+              {networkName}
+            </Button>
+          ) : (
+            <Button floated='right' icon labelPosition='left' onClick={activateBrowserWallet} primary>
+              <Icon name='plus square'></Icon>
+              Connect MetaMask
+            </Button>
+          )}
+        </Menu.Menu>
+      </Menu>
+      <br />
+      <br />
+      {children}
+    </Container>
+  );
+};
+
+export default PageWrapper;
